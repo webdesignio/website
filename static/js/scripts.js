503b23c965a3deffb692b1d51c577ba3add2349b
Vasil Krustev <vasil.krustev@me.com>
2016-09-23 13:13:35 +0300

    bug fix

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -1,5 +1,7 @@
 $(function() {
 
+  // Scroll Buttons
+
   function scrollToE(target, speed) {
     $('html, body').animate({
       scrollTop: $(target).offset().top
@@ -13,5 +15,26 @@ $(function() {
   $('.sad-penguin .arrow-down').click(function() {
     scrollToE('.sad-penguin', 300);
   });
+
+
+  // Values clearfix
+
+  function valuesClearfix() {
+    var w = $(window).width();
+
+    $('.values-container .clearfix').remove();
+
+    if (w > 767) {
+      alert('op');
+      $('.values-container li:nth-child(2n)').after( '<div class="clearfix"></div>' );
+    };
+
+  }
+
+  valuesClearfix();
+
+  $(window).resize(function() {
+    valuesClearfix();
+  })
 
 })
